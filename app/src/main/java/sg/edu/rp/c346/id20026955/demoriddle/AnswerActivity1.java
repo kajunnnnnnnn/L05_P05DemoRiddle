c26bb7d093f524d914a931023a7a489208ac3a69
20026955 <20026955@myrp.edu.sg>
2022-05-19 13:12:56 +0800

    debug done and additional challenge (ws)

--- app/src/main/java/sg/edu/rp/c346/id20026955/demoriddle/AnswerActivity1.java
+++ app/src/main/java/sg/edu/rp/c346/id20026955/demoriddle/AnswerActivity1.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 public class AnswerActivity1 extends AppCompatActivity {
@@ -15,10 +16,52 @@ public class AnswerActivity1 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_answer1);
 
+        Log.d("AnswerActivity1", "onCreate() called.");
+
         tvAnswer = findViewById(R.id.textViewAns1);
 //        tvAnswer.setText("In Second Activity");
         Intent intentReceived = getIntent();
-        String questionsSelectd = intentReceived.getStringExtra("Questions");
-        tvAnswer.setText(questionsSelectd + " answer is : Queue");
+        String questionsSelected = intentReceived.getStringExtra("Questions");
+//        tvAnswer.setText(questionsSelected + " answer is : Queue");
+
+        String msg = "";
+        if (questionsSelected.equals("Q1")){
+            msg = questionsSelected + " answer is: Queue";
+        } else if (questionsSelected.equals("Q2")){
+            msg = questionsSelected + " answer is: Gone";
+        }
+        tvAnswer.setText(msg);
     }
+
+    @Override
+    protected void onStart() {
+        Log.d("AnswerActivity1", "onStart() called.");
+        super.onStart();
+    }
+    @Override
+    protected void onResume() {
+        Log.d("AnswerActivity1", "onResume() called.");
+        super.onResume();
+    }
+    @Override
+    protected void onPause() {
+        Log.d("AnswerActivity1", "onPause() called.");
+        super.onPause();
+    }
+    @Override
+    protected void onStop() {
+        Log.d("AnswerActivity1", "onStop() called.");
+        super.onStop();
+    }
+    @Override
+    protected void onDestroy() {
+        Log.d("AnswerActivity1", "onDestroy() called.");
+        super.onDestroy();
+    }
+    @Override
+    protected void onRestart() {
+        Log.d("AnswerActivity1", "onRestart() called.");
+        super.onRestart();
+    }
+
 }
--- app/src/main/java/sg/edu/rp/c346/id20026955/demoriddle/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id20026955/demoriddle/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -18,6 +19,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.d("MainActivity", "onCreate() called.");
 
         tvQ1 = findViewById(R.id.textViewQ1);
         btnRevealQ1 = findViewById(R.id.buttonRevealQ1);
@@ -34,11 +37,41 @@ public class MainActivity extends AppCompatActivity {
         btnRevealQ2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, AnswerActivity2.class);
-                intent.putExtra("Questions", "Q2");
-                startActivity(intent);
+                Intent intent2 = new Intent(MainActivity.this, AnswerActivity1.class);
+                intent2.putExtra("Questions", "Q2");
+                startActivity(intent2);
             }
         });
-
     }
+    @Override
+    protected void onStart() {
+        Log.d("MainActivity", "onStart() called.");
+        super.onStart();
+    }
+    @Override
+    protected void onResume() {
+        Log.d("MainActivity", "onResume() called.");
+        super.onResume();
+    }
+    @Override
+    protected void onPause() {
+        Log.d("MainActivity", "onPause() called.");
+        super.onPause();
+    }
+    @Override
+    protected void onStop() {
+        Log.d("MainActivity", "onStop() called.");
+        super.onStop();
+    }
+    @Override
+    protected void onDestroy() {
+        Log.d("MainActivity", "onDestroy() called.");
+        super.onDestroy();
+    }
+    @Override
+    protected void onRestart() {
+        Log.d("MainActivity", "onRestart() called.");
+        super.onRestart();
+    }
+
 }
